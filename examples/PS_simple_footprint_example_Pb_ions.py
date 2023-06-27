01b15fb8f0a9238d6b9a252fb20d578d6d6eb3ca
elwaagaa <elias.walter.waagaard@cern.ch>
2023-06-27 11:56:37 +0200

    Line to save tune footprint

--- examples/PS_simple_footprint_example_Pb_ions.py
+++ examples/PS_simple_footprint_example_Pb_ions.py
@@ -52,3 +52,4 @@ print("\ndQx = {:.4f}, dQy = {:.4f}".format(dQx, dQy))
 #### Plot the tune footprint ####
 fig = plt.figure(figsize=(8, 8))
 PS_tune_footprint.generate_tune_footprint(fig, s)
+fig.savefig('output/PS_Pb_tune_footprint.png', dpi=250)
